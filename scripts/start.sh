8cffb976da9350a780e0a93d2eacc7a150636e60
Hszoo <trre1827151@gmail.com>
2025-06-02 21:48:29 +0900

    dd

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,14 +2,20 @@
 
 echo "--------------- STARTING NEW CONTAINER -----------------"
 
-# Pull latest image from ECR
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 536697254327.dkr.ecr.ap-northeast-2.amazonaws.com
+# ECR 로그인
+aws ecr get-login-password --region ap-northeast-2 \
+  | docker login --username AWS --password-stdin 536697254327.dkr.ecr.ap-northeast-2.amazonaws.com
+
+# 이미지 Pull
 docker pull 536697254327.dkr.ecr.ap-northeast-2.amazonaws.com/talkpick-server:latest
 
-# Run new container
+# 이전 컨테이너 삭제 (선택 사항)
+docker rm -f TalkPick_Server || true
+
+# 새 컨테이너 실행
 docker run -d \
   --name TalkPick_Server \
-  -p 8080:8080 \
+  -p 3000:3000 \
   --env-file /home/ubuntu/TalkPick_Server_CICD_Test/.env \
   536697254327.dkr.ecr.ap-northeast-2.amazonaws.com/talkpick-server:latest
 
