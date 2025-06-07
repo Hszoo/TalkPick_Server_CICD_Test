7191c6f2d8042a35357b1ac2e2ecea23cec7aa1d
Hszoo <trre1827151@gmail.com>
2025-06-07 17:36:49 +0900

    dockerfile

--- scripts/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "--------------- CLEANING PREVIOUS FILES -----------------"
-
-# Clean up previous files if any
-rm -rf /home/ubuntu/TalkPick_Server_CICD_Test/app.tar.gz || true
-
-echo "--------------- CLEAN COMPLETE -----------------"
--- scripts/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "--------------- STOPPING OLD CONTAINER -----------------"
-
-docker stop TalkPick_Server || true
-docker rm TalkPick_Server || true
-
-echo "--------------- CONTAINER STOPPED -----------------------"
